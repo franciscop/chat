80857db2eb0ac6bf9f79781424fc6dd719325e36
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-04-24 13:33:12 +0200

    updated

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import io from 'socket.io-client';
-import cookies from 'cookiesjs';
 
 import Chat from './chat.jsx';
 
-render((
-  <Chat
-    user={cookies('user')}
-    setUser={user => cookies({ user: user })}
-    socket={io()}
-  />
-), document.querySelector('#react-root'));
+render(<Chat />, document.querySelector('#react-root'));
